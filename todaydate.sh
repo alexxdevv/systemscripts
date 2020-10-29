720d0a9e53b237b2ffdd21a4f336888914e96a36
KikicraftMC <kikicraftgamer@gmail.com>
2020-10-29 09:08:22 +0100

    Create todaydate.sh

---
+++ todaydate.sh
@@ -0,0 +1,21 @@
+clear 
+echo 
+echo ████████████  ¡Welcome to TodayDate!  ████████████
+printf "        \e[1;77m\e[41m  Tool developed by @alexxdevv  \e[0m\n"
+echo 
+echo Please, wait 5 seconds to say you date!.
+echo
+sleep 5
+clear
+echo 
+printf "        \e[1;77m\e[41m Today is: \e[0m\n"
+echo
+echo "      $(date)"
+echo
+echo
+echo
+read -p "Finally press enter to exit"
+clear
+echo See you!
+sleep 2
+clear
